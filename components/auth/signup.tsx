2cbd87cbc312a05361e75086d98f64f93f5ea1dd
Lopun <lopun.jh@gmail.com>
2024-09-09 11:13:13 +0900

    Formik + Yup 조합으로 signup input validation 추가

--- components/auth/signup.tsx
+++ components/auth/signup.tsx
@@ -2,17 +2,49 @@
 
 import { Button, Input } from "@material-tailwind/react";
 import { useMutation } from "@tanstack/react-query";
-import { useState } from "react";
+import { useFormik } from "formik";
+import { useMemo, useState } from "react";
 import { createBrowserSupabaseClient } from "utils/supabase/client";
+import * as Yup from "yup";
 
 export default function SignUp({ setView }) {
-  const [otp, setOtp] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [confirmationRequired, setConfirmationRequired] = useState(false);
+
+  const validationSchema = useMemo(() => {
+    if (confirmationRequired) {
+      return Yup.object().shape({
+        otp: Yup.string().required().length(6, "6자리 숫자를 입력해주세요."),
+      });
+    } else {
+      return Yup.object().shape({
+        email: Yup.string()
+          .email("이메일 형식이 올바르지 않습니다.")
+          .required("이메일을 입력해주세요."),
+        password: Yup.string()
+          .min(8, "8자 이상 입력해주세요.")
+          .required("비밀번호를 입력해주세요."),
+      });
+    }
+  }, [confirmationRequired]);
 
   const supabase = createBrowserSupabaseClient();
   // signup mutation
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      otp: "",
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      if (confirmationRequired) {
+        verifyOtpMutation.mutate();
+      } else {
+        signupMutation.mutate();
+      }
+    },
+  });
 
   const signInWithKakao = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
@@ -29,8 +61,8 @@ export default function SignUp({ setView }) {
   const signupMutation = useMutation({
     mutationFn: async () => {
       const { data, error } = await supabase.auth.signUp({
-        email,
-        password,
+        email: formik.values.email,
+        password: formik.values.password,
         options: {
           emailRedirectTo: "http://localhost:3000/signup/confirm",
         },
@@ -50,8 +82,8 @@ export default function SignUp({ setView }) {
     mutationFn: async () => {
       const { data, error } = await supabase.auth.verifyOtp({
         type: "signup",
-        email,
-        token: otp,
+        email: formik.values.email,
+        token: formik.values.otp,
       });
 
       if (data) {
@@ -65,44 +97,46 @@ export default function SignUp({ setView }) {
   });
 
   return (
-    <div className="flex flex-col gap-4">
+    <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
       <div className="pt-10 pb-6 px-10 w-full flex flex-col items-center justify-center max-w-lg border border-gray-400 bg-white gap-2">
         <img src={"/images/inflearngram.png"} className="w-60 mb-6" />
         {confirmationRequired ? (
-          <Input
-            value={otp}
-            onChange={(e) => setOtp(e.target.value)}
-            label="otp"
-            type="text"
-            className="w-full rounded-sm"
-            placeholder="6자리 OTP를 입력해주세요."
-          />
+          <>
+            <Input
+              value={formik.values.otp}
+              onChange={formik.handleChange}
+              label="otp"
+              name="otp"
+              type="text"
+              className="w-full rounded-sm"
+              placeholder="6자리 OTP를 입력해주세요."
+            />
+            <span className="text-red-700">{formik.errors.otp}</span>
+          </>
         ) : (
           <>
             <Input
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={formik.values.email}
+              onChange={formik.handleChange}
               label="email"
+              name="email"
               type="email"
               className="w-full rounded-sm"
             />
+            <span className="text-red-700">{formik.errors.email}</span>
             <Input
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              value={formik.values.password}
+              onChange={formik.handleChange}
               label="password"
+              name="password"
               type="password"
               className="w-full rounded-sm"
             />
+            <span className="text-red-700">{formik.errors.password}</span>
           </>
         )}
         <Button
-          onClick={() => {
-            if (confirmationRequired) {
-              verifyOtpMutation.mutate();
-            } else {
-              signupMutation.mutate();
-            }
-          }}
+          type="submit"
           loading={
             confirmationRequired
               ? verifyOtpMutation.isPending
@@ -135,6 +169,6 @@ export default function SignUp({ setView }) {
           로그인하기
         </button>
       </div>
-    </div>
+    </form>
   );
 }
